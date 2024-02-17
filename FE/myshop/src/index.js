64e00baae2ea1fb7d0ae162d063d0a681d404a95
Ngansan <21521174@gm.uit.edu.vn>
2024-02-17 12:08:51 +0700

    Done create layout

--- FE/myshop/src/index.js
+++ FE/myshop/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HomePage from './pages/users/homePage';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+  <HomePage/>
 );
 
 // If you want to start measuring performance in your app, pass a function
